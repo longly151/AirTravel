b2a943b2627d1ebfeb9758ef24da9e1347928b72
Le Viet Long <vilagondk@gmail.com>
2020-10-08 13:58:24 +0700

    Upgrade to RN63.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  // extends: '@react-native-community',
+  // parser: '@typescript-eslint/parser',
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'only-warn'],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'react/static-property-placement': 0,
+    'react/jsx-props-no-spreading': 0,
+    'global-require': 0,
+    'no-param-reassign': 0,
+    'object-curly-newline': 'off',
+    'react/jsx-closing-bracket-location': [
+      1,
+      {selfClosing: 'line-aligned', nonEmpty: 'after-props'},
+    ],
+  },
 };
--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {
+  AppRegistry,
+  YellowBox,
+} from 'react-native';
+import moment from 'moment';
+// import App from './src/app/app';
 import App from './App';
-import {name as appName} from './app.json';
+import {
+  name as appName,
+} from './app.json';
+import 'react-native-gesture-handler';
+import 'moment/locale/vi';
 
+moment.locale('vi');
+
+YellowBox.ignoreWarnings([
+  'Cannot update during an existing state transition',
+  'Require cycle: node_modules/',
+]);
 AppRegistry.registerComponent(appName, () => App);
