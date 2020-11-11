8549673ec436191dc98ff68017d686d063e91437
Your Name <you@example.com>
2020-11-11 10:40:07 +0000

    Merge

--- src/core/config/sagas.ts
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import authSaga from '@contents/Auth/redux/saga';
-import productSaga from '@contents/Example/containers/Common/FlatList/WithRedux/redux/saga';
-
-export default function* root() {
-  yield all([...authSaga, ...productSaga]);
-}
